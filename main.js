9d512f6ccab7abd4aec0c28b37461adc687491c4
Jasmine Walters <jsmnwltrs@gmail.com>
2018-09-20 16:14:29 -0500

    added functionality to build and delete cards

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
-const PrintToDom = (domString, divId) => {
-    let domElement = document.getElementById(divId);
-    domElement.innerHTML = domString;
+const nameInput = document.getElementById('inputName');
+const sortButton = document.getElementById('sortButton');
+
+const printToDom = (string, divId) => {
+    document.getElementById(divId).innerHTML += string;
 };
+
+const activateExpel = () => {
+document.querySelector("body").addEventListener("click", function(event) {
+  if (event.target.classList.contains('expelButton')) {
+    const buttonIClicked = event.target;
+      const cardToExpel = buttonIClicked.parentNode.parentNode;
+      cardToExpel.remove();
+  }
+})
+};
+
+const cardBuilder = (name) => {
+    let sortingHouse = '';
+    let randomizer = Math.floor((Math.random() * 4) + 1);
+        if (randomizer === 1) {
+            sortingHouse = 'Gryffindor'
+        };
+        if (randomizer === 2) {
+            sortingHouse = 'Slytherin'
+        };
+        if (randomizer === 3) {
+            sortingHouse = 'Huffelpuff'
+        };
+        if (randomizer === 4) {
+            sortingHouse = 'Ravenclaw'
+        };
+    let domString = `<div class="card w-25 m-2">
+        <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <h6 class="card-text">${sortingHouse}!</h6>
+            <a href="#" class="btn btn-primary expelButton">Expel!</a>
+         </div>
+     </div>`;
+    printToDom(domString, 'studentCards');
+    activateExpel();
+}
+
+sortButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    cardBuilder(nameInput.value);
+});
 
